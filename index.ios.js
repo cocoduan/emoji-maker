2c3f98d1d3d971c9bc09001ad0919af2f20a7c02
Coco Duan <dhh1216@gmail.com>
2017-03-08 18:18:35 -0800

    Capture and then display image

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,89 @@
 /**
- * Sample React Native App
+ * Emoji Maker App
  * https://github.com/facebook/react-native
- * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+    Image
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class emojiMaker extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageSrc: ""
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {!this.state.imageSrc && (<Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}
+                    captureTarget={Camera.constants.CaptureTarget.disk}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>)}
+
+                {this.state.imageSrc!== "" && (<Image
+                    source={{
+                        isStatic: true,
+                        uri: this.state.imageSrc
+                    }}
+                    style={styles.image}
+                />)}
+            </View>
+        );
+    }
+
+    takePicture() {
+        this.camera.capture()
+            .then((data) => {
+                this.setState({
+                    imageSrc: data.path
+                });
+                console.log(data);
+            })
+            .catch(err => console.error(err));
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#F5FCFF'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    },
+    image: {
+        width: 200,
+        height: 200,
+        resizeMode: 'center'
+    }
 });
 
 AppRegistry.registerComponent('emojiMaker', () => emojiMaker);
